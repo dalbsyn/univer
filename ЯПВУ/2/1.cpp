5b3cd7e9120e18b03425b800f845729c18d60cee
dalbsyn <rmrfmanwoman@gmail.com>
2025-09-13 20:58:27 +0500

    япву-лр2: начало лабораторных работ на cpp

---
+++ ЯПВУ/2/1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    std::cout << "\"To be\n";
+    std::cout << "\t or not\n";
+    std::cout << "\t\t to be?\"\n";
+    std::cout << "\t\\Shakespeare\\";
+}
---
+++ ЯПВУ/2/2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    std::wcout << "Name:  \"Меткие стрелки\"\n";
+    std::wcout << "Avtor: \"Теодор Гриц\"\n";
+    std::wcout << "Izdatelstvo: \"Детгиз\"\n";
+    std::wcout << "Pages: 447\n";
+}
