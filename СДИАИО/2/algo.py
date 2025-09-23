7f11e952d86263fcc8f47c13e4ae42c443975489
dalbsyn <rmrfmanwoman@gmail.com>
2025-09-23 10:02:19 +0500

    сдиаио-лр2: полностью выполненное задание

--- СДИАИО/2/algo.py
+++ СДИАИО/2/algo.py
@@ -1,25 +1,89 @@
-def process_input(input: str):
-    input = input
-    output = []
-    print(input)
-    processed_input = input.split(' ')
-    print(processed_input)
-    for i in processed_input:
+def process_output(text):
+    """Обработка входных данных - полученный string превращает в список, где
+    каждый элемент списка - строка (line).
+
+    Принимает string и возвращает список, где каждый элемент - список, который
+    состоит из string и количества слов в текущем string.
+    """
+    splitted_text = text.split(' ')
+    line_list = []
+    for i in splitted_text:
         try:
-            output.append(int(i))
+            line_list.append(int(i))
         except ValueError:
             return 0
-    return output
+    return line_list
 
-def sort(input):
-    input = process_input(input)
-    processed_input = []
-    if input == 0:
-        return 0
-    else:
-        for i in range(len(input)):
-            for j in range(i + 1, len(input)):
-                if input[i] < input[j]:
-                    print(f'{input[i]} < {input[j]}')
-                    processed_input.append(input[i])
-    return processed_input
+def find_all_increasing_subsequences(arr):
+    """
+    Поиск и сохранение всех подпоследовательностей.
+    """
+    subsequences = []
+    
+    """
+    Генератор подпоследовательностей на основе рекурсивной функции.
+    Если текущая подпоследовательность увеличивается, то начинается следующая итерация.
+    Если не увеличивается, то эта подпоследовательность сохраняется.
+    """
+    def generate_subsequences(current_seq, remaining_arr):
+        if not remaining_arr:
+            # Check if the generated subsequence is increasing
+            is_increasing = True
+            for i in range(1, len(current_seq)):
+                if current_seq[i] <= current_seq[i-1]:
+                    is_increasing = False
+                    break
+            
+            if is_increasing and len(current_seq) > 0:
+                subsequences.append(current_seq)
+            return
+
+        # Включить текущий элемент
+        generate_subsequences(current_seq + [remaining_arr[0]], remaining_arr[1:])
+        # Исключить текущий элемент
+        generate_subsequences(current_seq, remaining_arr[1:])
+    
+    generate_subsequences([], arr)
+    return subsequences
+
+def find_longest_increasing_subsequence(arr):
+    """
+    Поиск наибольшей возрастающей подпоследовательности (нвп).
+    """
+    if not arr:
+        return []
+
+    n = len(arr)
+    dp = [1] * n  # список количества элементов всех подпоследовательностей
+    prev_index = [-1] * n  # вспомогательный список для бектрекинга, где каждый элемент - указатель к прошлому элементу (поэтому -1), подобие флагового значения.
+
+    """
+    Если текущий элемент подпоследовательности больше, чем предыдущий, то это 
+    засчитывается в количество чисел текущей подпоследовательности.
+    """
+    for i in range(1, n):
+        for j in range(i):
+            if arr[i] > arr[j] and dp[j] + 1 > dp[i]:
+                dp[i] = dp[j] + 1
+                prev_index[i] = j
+
+    # Поиск индекса нвп
+    max_length = 0
+    end_index = -1
+    for i in range(n):
+        if dp[i] > max_length:
+            max_length = dp[i]
+            end_index = i
+
+    """
+    Реконструкция нвп с помощью бектрекинга.
+    Сперва находится число нвп, от которого идем обратно к началу последовательности, пока не достигнем флагового значения
+    """
+    lis = []
+    if end_index != -1:
+        current_index = end_index
+        while current_index != -1:
+            lis.insert(0, arr[current_index])
+            current_index = prev_index[current_index]
+
+    return lis
--- СДИАИО/2/main.py
+++ СДИАИО/2/main.py
@@ -1,53 +1,104 @@
-import sys
-from PySide6.QtWidgets import (QApplication, QVBoxLayout, QLineEdit, QWidget,
-                               QPushButton, QTableWidget, QTableWidgetItem,
-                               QLabel, QMessageBox)
+from PySide6.QtWidgets import (QApplication, QWidget, QLineEdit, QLabel,
+                                QPushButton, QVBoxLayout, QMessageBox, 
+                                QTableWidget, QTableWidgetItem)
+from PySide6.QtGui import QColor
 import algo
 
 class Window(QWidget):
     def __init__(self):
+        """Инициализация интерфейса программы.
+        """
         super().__init__()
-        self.setWindowTitle("ЗАГОЛОВИЧИЩЕ ОКНОШИЩА")
         self.init_layout()
         self.init_widgets()
         self.place_widgets()
         self.connections()
-    
+        self.customize_table()
+
     def init_layout(self):
+        """Инициализация макета интерфейса
+        """
         self.main_layout = QVBoxLayout()
         self.setLayout(self.main_layout)
-    
+
     def init_widgets(self):
-        self.label_input_list = QLabel("Введите список чисел")
-        self.label_warning = QLabel("!!!!!СОРТИРУЕТ ТОЛЬКО ПО ВОЗРАСТАНИБЮ!!!!!!!1")
-        self.button = QPushButton("ПУКС")
-        self.field_input = QLineEdit()
-        self.table = QTableWidget(100, 2)
+        """Инициализация всех виджетов
+        """
+        self.label_input_list = QLabel('Введите массив чисел')
+        self.label_output_list = QLabel('Все найденные массивы чисел. Красным отмечается наибольшая подпоследовательность')
+        self.input_field = QLineEdit()
+        self.output_box = QTableWidget(200, 2)
+        self.sort_button = QPushButton('!НАЙТИ!')
     
     def place_widgets(self):
+        """Расположение виджетов на макете
+        """
         self.main_layout.addWidget(self.label_input_list)
-        self.main_layout.addWidget(self.field_input)
-        self.main_layout.addWidget(self.button)
-        self.main_layout.addWidget(self.table)
-        self.main_layout.addWidget(self.label_warning)
+        self.main_layout.addWidget(self.input_field)
+        self.main_layout.addWidget(self.label_output_list)
+        self.main_layout.addWidget(self.output_box)
+        self.main_layout.addWidget(self.sort_button)
     
     def connections(self):
-        self.button.clicked.connect(self.find_in_list)
-    
-    def find_in_list(self):
-        self.parse_input()
+        """Инициализация триггеров на действия от элементов интерфейса
+        """
+        self.sort_button.clicked.connect(self.solution)
 
-    def parse_input(self):
-        self.input = self.field_input.text()
-        self.processed_input = algo.sort(self.input)
+    def solution(self):
+        """Решение задачи
+        """
+        self.input = self.input_field.text()
+        self.processed_input = algo.process_output(self.input)
         if self.processed_input == 0:
-            QMessageBox(text='''ОШИБКА: ВВОДИТЕ ТОЛЬКО ЧИСЛА.
-НАРУЖЕНЫ НЕДОПУСТИМЫЕ СИМВОЛЫ (в т.ч. пробел).
-НЕОБРАБАТЫВАЕМОЕ ИСКЛЮЧЕНИЕ.''').exec()
-        print(self.processed_input)
+            QMessageBox(text='УБЕРИТЕ КАКИЕ-ЛИБО НЕ ЧИСЛА').exec()
+        else:
+            self.output_box.clear()
+            self.fill_table(self.processed_input)
+            self.add_longest(self.processed_input)
+            self.color_row()
+    
+    def add_longest(self, list):
+        """
+        Добавление нвп
+        """
+        self.result = algo.find_longest_increasing_subsequence(list)
+        print(f"Наибольшая подпоследовательность: {len(self.result)} - {self.result}")
+        self.output_box.setItem(0, 0, QTableWidgetItem(str(len(self.result))))
+        self.output_box.setItem(0, 1, QTableWidgetItem(str(self.result)))
 
+    def fill_table(self, list):
+        """
+        Заполнение таблицы остальными подпоследовательностями
+        """
+        self.subseq = algo.find_all_increasing_subsequences(list)
+        self.counter = 1
+        for i in self.subseq:
+            print(f"Добавляется подпоследовательность и количество элементов: {len(i)} - {i}")
+            self.output_box.setItem(self.counter, 0, QTableWidgetItem(str(len(i))))
+            self.output_box.setItem(self.counter, 1, QTableWidgetItem(str(i)))
+            self.counter += 1
+    
+    def color_row(self):
+        """
+        Разукрашивание нвп
+        """
+        self.color: QColor = QColor("#FF2A2A")
+        self.item0 = self.output_box.item(0, 0)
+        self.item1 = self.output_box.item(0, 1)
 
-app = QApplication(sys.argv)
+        # Чтоб VSCode не ныл, что в нужной строке ничего нет (None).
+        # Все там будет, поэтому пусть заткнется.
+        assert self.item0
+        assert self.item1
+
+        self.item0.setBackground(self.color)
+        self.item1.setBackground(self.color)
+    
+    def customize_table(self):
+        self.output_box.setHorizontalHeaderLabels(['Количество элементов', 'Подпоследовательность'])
+        self.output_box.resizeColumnsToContents()
+
+app = QApplication()
 window = Window()
 window.show()
-sys.exit(app.exec())
+app.exec()
