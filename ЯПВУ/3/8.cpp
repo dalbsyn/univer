2df190e320620a4555a25d7fd385bca299aa6b05
dalbsyn <rmrfmanwoman@gmail.com>
2025-09-25 21:58:56 +0500

    япву-лр3: продолжение работы

--- ЯПВУ/3/8.cpp
+++ ЯПВУ/3/8.cpp
@@ -2,14 +2,32 @@
 #include <cmath>
 using namespace std;
 
+double side(double a, double h){
+    return 5 * a * h;
+}
+
+double base_calc(double a){
+    double under_sqrt, square, result;
+    under_sqrt = 5 * (5 + 2 * sqrt(5));
+    wcout << under_sqrt << endl;
+    wcout << sqrt(under_sqrt) << endl;
+    wcout << pow(a ,2) << endl;
+    result = ((1 / 2) * sqrt(under_sqrt) * pow(a, 2));
+    wcout << result;
+    return result;
+}
 int main(){
-    double a, b, h, surface;
-    wcout << "Введите значение для a: ";
+    setlocale(LC_ALL, "");
+    double a, h, surface, base;
+    wcout << L"Введите значение для a: ";
     wcin >> a;
-    wcout << "Введите значение для b: ";
-    wcin >> b;
-    wcout << "Введите значение для h: ";
+    wcout << L"Введите значение для h: ";
     wcin >> h;
-    surface =  (5 * a *b + 5 * b * h);
-    wcout << "Площадь: " << surface << endl;
+
+    surface = side(a, h);
+    base = base_calc(a);
+
+    wcout << L"Площадь боковой поверхности: " << surface << endl;
+    wcout << L"Площадь основания: " << base << endl;
+
 }
