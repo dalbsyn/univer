a8f87766cb5c93337146c6ec43fc35b831fad733
dalbsyn <rmrfmanwoman@gmail.com>
2025-10-30 20:43:07 +0500

    сдиаио-4: пояснение к алгоритму Дейкстры

--- СДИАИО/4/dijkstra.py
+++ СДИАИО/4/dijkstra.py
@@ -1,30 +1,51 @@
 import heapq
-import networkx as nx
-import matplotlib.pyplot as plt
  
 def dijkstra(graph, start, end):
-    # Initialize distances dictionary
+    '''
+    Словарь хранения кратчайшего пути от начальной вершины до каждой.
+    Все вершины изначально имеют бесконечный (неизвестный) вес.
+    '''
     distances = {node: float('inf') for node in graph}
     distances[start] = 0
+    print(f'dijkstra.distances = {distances}')
      
-    # Initialize priority queue
+    '''
+    Инициализация кучи - структуры данных, представляющей из себя бинарное
+    дерево в линейном представлении. По умолчанию используется минимальная
+    куча, в которой корень - минимальное число, что в данном случае особенно
+    удобно, так как начальной вершиной является всегда 0, а остальные - 
+    бесконечные, так как они неизвестны пока что.
+    ''' 
     pq = [(0, start)]
      
-    # Initialize previous node dictionary to store shortest path
+    '''
+    Словарь хранения предка для каждой вершины. Используется для 
+    реконструкции пути.
+    '''
     previous = {}
      
     while pq:
+        '''
+        Цикл поиска минимального расстояния между двумя вершинами. Отчасти, эта
+        задача и решается с помощью введенной выше кучи.
+        '''
         current_distance, current_node = heapq.heappop(pq)
          
-        # If we already found a shorter path to the current node, skip
+        # Если уже был найден короткий путь, то пропускаем
         if current_distance > distances[current_node]:
             continue
          
-        # Stop if we reached the destination
+        # Остановка, если достигли конца
         if current_node == end:
             break
          
-        # Explore neighbors
+        '''
+        Сбор информации о соседних узлах.
+        Сперва вычисляется состояние от текущего узла до соседнего узла.
+        Если новое расстояние короче, чем текущее записанное, то расстояние
+        обновляется и это же расстояние добавляется в кучу, и соседний узел
+        становится предком.
+        '''
         for neighbor, distance in graph[current_node].items():
             new_distance = current_distance + distance
             if new_distance < distances[neighbor]:
@@ -32,7 +53,11 @@ def dijkstra(graph, start, end):
                 heapq.heappush(pq, (new_distance, neighbor))
                 previous[neighbor] = current_node
      
-    # Reconstruct the shortest path
+    '''
+    Реконструкция пути с помощью словаря previous.
+    Так как путь получается от конца к началу, то полученный список 
+    переворачивается.
+    '''
     path = []
     node = end
     while node != start:
@@ -41,4 +66,4 @@ def dijkstra(graph, start, end):
     path.append(start)
     path.reverse()
      
-    return distances[end], path
+    return distances[end], path 
