1fdb2dbf83bb89c7a2c1b89048431211918887d1
dalbsyn <rmrfmanwoman@gmail.com>
2026-01-02 16:35:13 +0500

    общее: простой nix-shell

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  # We pin to a specific nixpkgs commit for reproducibility.
+  # Last updated: 2024-04-29. Check for new commits at https://status.nixos.org.
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.11") {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+      # select Python packages here
+      pyside6
+      matplotlib
+      networkx
+    ]))
+  pkgs.gcc15];
+}
